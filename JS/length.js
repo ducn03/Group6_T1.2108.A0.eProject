d21686bd1df25eda6d534e2bb267b72d7564f3a5
ducn03 <94164852+ducn03@users.noreply.github.com>
2021-11-28 00:48:25 +0700

    Delete JS directory

--- JS/length.js
+++
@@ -1,8 +0,0 @@
-function LengthConverter(valNum) {
-    document.getElementById("outputMeters").value=(valNum/3.2808).toFixed(2);
-    document.getElementById("outputInches").value=(valNum*12).toFixed(2);
-    document.getElementById("outputcm").value=(valNum/0.032808).toFixed(2);
-    document.getElementById("outputYards").value=(valNum*0.33333).toFixed(2);
-    document.getElementById("outputKilometers").value=(valNum/3280.8).toFixed(2);
-    document.getElementById("outputMiles").value=(valNum*0.00018939).toFixed(2);
-}
