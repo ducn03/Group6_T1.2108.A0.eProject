90cf42a71888c85933b3d751431aa01295ba4ce0
ducn03 <dinhduclt03@gmail.com>
2022-01-20 00:07:36 +0700

    Ducupdate

--- JS/length.js
+++ JS/length.js
@@ -1,15 +1,37 @@
-function LengthConverterft(valNum) {
-    document.getElementById("outputMeters").value=(valNum/3.2808).toFixed(2);
+const currencyEl_one = document.getElementById('currency-one');
+const currencyEl_two = document.getElementById('currency-two');
+const amountEl_one = document.getElementById('amount-one');
+const amountEl_two = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch exchange rates and update the dome
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value; 
+
+  fetch(`API/api-Length/${currency_one}.json`)
+    .then((res) => res.json())
+    .then((data) => {
+      //   console.log(data);
+      const rate = data.conversion_rates[currency_two];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(12)
+    });
 }
 
-function LengthConverterin(valNum) {
-    document.getElementById("outputMillimetre").value=(valNum*25.4).toFixed(2);
-};
+// Event Listeners
+currencyEl_one.addEventListener('change', calculate);
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
 
-function LengthConverteryd(valNum) {
-    document.getElementById("outputMeters").value=(valNum/1.0936).toFixed(2);
-};
-
-function LengthConvertermile(valNum) {
-    document.getElementById("outputKilometers").value=(valNum/0.62137).toFixed(6);
-};
+calculate();
